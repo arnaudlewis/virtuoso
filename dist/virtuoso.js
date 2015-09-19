5b14ad5030c464f99303a20da14833517c8fd26d
alw <alw@zengularity.com>
2015-09-20 00:19:19 +0200

    add dynamic wrapper on accordion

--- dist/virtuoso.js
+++ dist/virtuoso.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
   'use strict';
 
+  $.each($('.accordions'), function(index, component) {
+    $.each($(component).children(), function(index, child) {
+      var summaryNode = $($(child).find(".summary"));
+      var summaryContent = summaryNode.html();
+      summaryNode.html("");
+      summaryNode.append('<div class="content"></div>');
+      $(child).find('.summary .content').html(summaryContent);
+    });
+  });
+
   $('input[type=checkbox]').on('change', function () {
     if ($(this).is(':checked')) {
-      $($(this).closest('.item')).find('.summary').height($($(this).closest('.item')).find('.summary p').innerHeight());
+      $($(this).closest('.item')).find('.summary').height($($(this).closest('.item')).find('.summary .content').innerHeight());
     } else {
       $($(this).closest('.item')).find('.summary').height(0);
     }

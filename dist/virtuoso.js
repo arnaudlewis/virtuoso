f27b30c27b434edf455b557af529d51d2e41d19d
alw <alw@zengularity.com>
2015-11-12 11:38:07 +0100

    new interaction with the fixed menu bar

--- dist/virtuoso.js
+++ dist/virtuoso.js
@@ -68,10 +68,30 @@ $.each($('.alternated-highlights'), function (index, component) {
 });
 $( document ).ready(function() {
 
-  $('.fixed-menu').prepend('<span class="navbar-switcher menu-burger"></span>');
+  $('.fixed-menu-container').prepend('<span class="navbar-switcher menu-burger"></span>');
+  $('.fixed-menu').prepend('<div class="overlay"></div>');
 
   $('.fixed-menu').find('.navbar-switcher').on('click', function() {
-    $(this).parent().find('nav').toggleClass('active');
+    var $fixedMenu = $(this).closest('.fixed-menu');
+    $fixedMenu.find('nav').toggleClass('active');
+    $('body').toggleClass('freeze');
+
+    var overlayAction = function toggleOverlay() {
+      $overlay = $fixedMenu.find('.overlay');
+      
+      if($overlay.hasClass('animate')) {
+        $overlay.toggleClass('animate')
+        window.setTimeout(function() { $overlay.toggleClass('pre-animate') }, 300);
+      }
+      else {
+        $overlay.toggleClass('pre-animate')
+        window.setTimeout(function() { $overlay.toggleClass('animate') }, 50);
+        $overlay.one('click', function() {
+          $(this).parent().find('.navbar-switcher').trigger('click')
+        });
+      }
+    }()
+
   });
 
   var docElem = $(document)[0],
